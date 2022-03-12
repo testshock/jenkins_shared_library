4989387e92e772eac05d0e893f9dcc8cb8a3941b
testshock <mavrelos@protonmail.com>
2022-03-12 14:11:56 +0200

    Update buildUnity.groovy

--- vars/buildUnity.groovy
+++ vars/buildUnity.groovy
@@ -1,4 +1,7 @@
 def call(Map config = [:]) {
     //sh "/Applications/Unity/Hub/Editor/${config.unityVersion}/Unity.app/Contents/MacOS/Unity -logfile - -quit -buildTarget ${config.target} -batchmode -projectPath ${config.projectPath} -executeMethod BuildScript.PerformAndroidBuild"
     sh "echo Unity Version:${config.unityVersion}"
+    sh "echo Build Target:${config.buildTarget}"
+    sh "echo Target:${config.target}"
+    sh "echo Project:${config.project}"
 }
