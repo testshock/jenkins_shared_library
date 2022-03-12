81b94dcf3c90b21ccc4e7e69967d2643d2f54a4b
testshock <mavrelos@protonmail.com>
2022-03-12 15:50:31 +0000

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,13 +1,15 @@
 def call(Map config = [:]) {
     if (config.targetEnvironment == "Development") {
+        br = config.branch.minus("origin/")
         if (config.branch!="Development"){
             br = config.branch.minus("origin/")
         } else {
             br = "Development"
         }
-        echo "Checking out branch "+br
-        git branch: br, credentialsId: 'github-testshock-cmd', url: 'https://github.com/testshock/'+config.project
+        sh "echo Checking out branch ${br} for project:${config.project}"
+        //git branch: br, credentialsId: 'github-testshock-cmd', url: 'https://github.com/testshock/'+config.project
     } else {
-        checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: 'https://github.com/testshock/'+config.project, credentialsId: 'github-testshock-cmd' ]], branches: [[name: '${config.gittag}']]], poll: false
+//        checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: 'https://github.com/testshock/'+config.project, credentialsId: 'github-testshock-cmd' ]], branches: [[name: '${config.gittag}']]], poll: false
+        sh "echo tag:${config.gittag}|
     }
 }
