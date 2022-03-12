83f02673db282e0f897d77a8e52ddfe3c05070f9
testshock <mavrelos@protonmail.com>
2022-03-12 14:39:46 +0200

    Update build-unity.sh

--- resources/com/fmgames/scripts/build-unity.sh
+++ resources/com/fmgames/scripts/build-unity.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#!/bin/bash
 /Applications/Unity/Hub/Editor/$1/Unity.app/Contents/MacOS/Unity -logfile - -quit -buildTarget $2 -batchmode -projectPath $3 -executeMethod BuildScript.PerformAndroidBuild
