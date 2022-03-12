3c46d584f83d5100b405ad414ed0b7c8f03ffc2d
testshock <mavrelos@protonmail.com>
2022-03-12 14:30:01 +0200

    build

---
+++ resources/com/fmgames/scripts/build-unity.sh
@@ -0,0 +1,2 @@
+#!bin/bash
+/Applications/Unity/Hub/Editor/$1/Unity.app/Contents/MacOS/Unity -logfile - -quit -buildTarget $2 -batchmode -projectPath $3 -executeMethod BuildScript.PerformAndroidBuild
--- vars/buildUnity.groovy
+++ vars/buildUnity.groovy
@@ -1,7 +1,15 @@
 def call(Map config = [:]) {
-    //sh "/Applications/Unity/Hub/Editor/${config.unityVersion}/Unity.app/Contents/MacOS/Unity -logfile - -quit -buildTarget ${config.target} -batchmode -projectPath ${config.projectPath} -executeMethod BuildScript.PerformAndroidBuild"
+    // Get script from resources and save it in filesystem
+    def scriptcontents = libraryResource "com/fmgames/scripts/build-unity.sh"
+    writeFile file:"build-unity.sh", text:scriptcontents
+    sh "chmod a+x ./build-unity.sh"
+
+    // Execute the script with the following parameters    
     sh "echo Unity Version:${config.unityVersion}"
     sh "echo Build Target:${config.buildTarget}"
     sh "echo Target:${config.target}"
     sh "echo Project:${config.project}"
+    sh "./build-unity.sh ${config.unityVersion} ${config.target} ${config.projectPath}"
+
+    //sh "/Applications/Unity/Hub/Editor/${config.unityVersion}/Unity.app/Contents/MacOS/Unity -logfile - -quit -buildTarget ${config.target} -batchmode -projectPath ${config.projectPath} -executeMethod BuildScript.PerformAndroidBuild"
 }
